7808daec08e4039c06c5ef5f6a7812437b29b93f
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2020-12-09 11:23:12 -0600

    Initial Commit

---
+++ LinkedList_AscDsc/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LinkedList_AscDsc
+//
+//  Created by Prajwal Dhungana on 12/9/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
